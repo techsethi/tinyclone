c72b8f5480d52aa3eb2bc7ea7e4f2860a63cadfc
Pradeep Sethi <psethi@gmail.com>
2013-04-10 17:46:29 +0530

    generic username & password

--- tinyclone.rb
+++ tinyclone.rb
@@ -16,7 +16,7 @@ helpers do
 
   def authorized?
     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['tcity', '123@t!mes']
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['USERNAME', 'PASSWORD']
   end
 
   def date_format(input_date)
